0ae925f462090a8cffcd213b0f43a159c665c0b4
eyadqasim <aeadq2001@gmail.com>
2025-08-24 21:53:08 +0300

    Add GitHub Pages routing fix with 404.html and index.html script

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import App from './App.jsx'  // Import App, not Home
 import ProductDetails from './pages/ProductDetails.jsx'  
 import ContactPage from './pages/Contact.jsx'    
 import AboutUs from './pages/AboutUs.jsx'    
-import Products from './pages/Products.jsx'
-import Home from './pages/Home.jsx'
+import Products from './pages/Products.jsx'  // This should probably be Products, not ProductDetails
+import Home from './pages/Home.jsx' // Import Home from the correct location
+
+// Import your beautiful 404 page
 import Error404Page from './pages/Error404Page.jsx'
+
+// Import LanguageProvider
 import { LanguageProvider } from './context/LanguageContext'
 
 import './index.css'
 
+//...
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
@@ -36,8 +41,8 @@ Sentry.init({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
-      {/* Using HashRouter - no basename needed */}
-      <Router>
+      {/* CRITICAL: Must match GitHub repo name exactly */}
+      <Router basename="/Nelover-Landing-Page">
         <Routes>
           {/* Main Routes */}
           <Route path="/" element={<Home />} />
